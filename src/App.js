cf2d14aed478f1c8607ab8ee9d950f9a562f0ba7
Colin Clark <bitdepth@users.noreply.github.com>
2021-06-04 15:07:19 +0100

    Design tweaks

--- src/App.js
+++ src/App.js
@@ -1,75 +1,95 @@
-/** @jsxImportSource theme-ui */ 
-import {Link, Route, Switch} from 'react-router-dom';
-import { ThemeProvider, Button, Flex, Text} from 'theme-ui';
-import { ToastProvider } from 'react-toast-notifications'
-import {deep} from '@theme-ui/presets';
-import List from './components/list';
-import Upload from './components/upload';
-import {nanoid} from 'nanoid';
+/** @jsxImportSource theme-ui */
 
-const textShadow = '-0.0075em 0.0075em 0 #fef2f6, 0.005em 0.005em 0 #f6a5c1, 0.01em 0.01em 0 #f7aac4, 0.015em 0.015em #f7aec7, 0.02em 0.02em 0 #f8b3ca, 0.025em 0.025em 0 #f8b7cd, 0.03em 0.03em 0 #f8bcd0, 0.035em 0.035em 0 #f9c0d3'
+import { Link, Route, Switch } from "react-router-dom";
+import { ThemeProvider, Button, Flex, Text } from "theme-ui";
+import { ToastProvider } from "react-toast-notifications";
+import { deep } from "@theme-ui/presets";
+import List from "./components/list";
+import Upload from "./components/upload";
+import { nanoid } from "nanoid";
+
+const textShadow =
+  "-0.0075em 0.0075em 0 #fef2f6, 0.005em 0.005em 0 #f6a5c1, 0.01em 0.01em 0 #f7aac4, 0.015em 0.015em #f7aec7, 0.02em 0.02em 0 #f8b3ca, 0.025em 0.025em 0 #f8b7cd, 0.03em 0.03em 0 #f8bcd0, 0.035em 0.035em 0 #f9c0d3";
 
 const customTheme = {
   ...deep,
   buttons: {
     primary: {
-      '&:hover': {
-        cursor: 'pointer',
-      },
+      "&:hover": {
+        cursor: "pointer"
+      }
     },
     secondary: {
-      '&:hover': {
-        cursor: 'pointer',
-      },
-    },
-  },
-}
+      "&:hover": {
+        cursor: "pointer"
+      }
+    }
+  }
+};
 function App() {
-
-  if(!localStorage.getItem('CAT_SUB_ID')) {
-    localStorage.setItem('CAT_SUB_ID', nanoid())
+  if (!localStorage.getItem("CAT_SUB_ID")) {
+    localStorage.setItem("CAT_SUB_ID", nanoid());
   }
 
   return (
     <ThemeProvider theme={customTheme}>
       <ToastProvider>
-    
-    <Flex sx={{
-      backgroundColor: 'secondary'
-    }} mb={40}>
-
-    <Text as="h1" sx={{
-      	color: 'mix(#fff, #e91e63, 85%)',
-        fontSize: [25, 50, 150],
-        fontWeight: 900,
-        textShadow: textShadow,
-        transform: 'rotate(337deg)'
-    }}>Catify</Text>
-      <Button sx={{flex: 1}}><Link sx={{
-        color: 'background',
-        textDecoration:'none',
-        fontSize: [20, 40, 80],
-        fontFamily: 'heading'
-        }} to="/">Home</Link></Button>
-      <Button sx={{
-        flex: 1, 
-        }}><Link to="/upload" sx={{
-          color: 'background',
-          textDecoration:'none',
-          fontSize: [20, 40, 80],
-          fontFamily: 'heading'
-          }}>Upload</Link></Button>
-    </Flex>
-    <Switch>
-      <Route path="/upload">
-        <Upload />
-      </Route>
-      <Route path="/">
-        <List />
-      </Route>
-    </Switch>
-    </ToastProvider>
-  </ThemeProvider>
+        <Flex
+          sx={{
+            backgroundColor: "secondary",
+            borderRadius: 100
+          }}
+          m={40}
+        >
+          <Link
+            to="/"
+            sx={{
+              color: "mix(#fff, #e91e63, 85%)",
+              textDecoration: "none"
+            }}
+          >
+            <Text
+              as="h1"
+              sx={{
+                color: "mix(#fff, #e91e63, 85%)",
+                fontSize: [25, 50, 150],
+                fontWeight: 900,
+                textShadow: textShadow,
+                transform: "rotate(327deg)"
+              }}
+            >
+              Catify
+            </Text>
+          </Link>
+          <Button
+            sx={{
+              flex: 1,
+              borderRadius: 100
+            }}
+          >
+            <Link
+              to="/upload"
+              sx={{
+                color: "background",
+                textDecoration: "none",
+                fontSize: [20, 40, 80],
+                fontFamily: "heading"
+              }}
+            >
+              Upload
+            </Link>
+          </Button>
+        </Flex>
+        <Switch>
+          <Route path="/upload">
+            <Upload />
+          </Route>
+          <Route path="/">
+            <List />
+          </Route>
+        </Switch>
+      </ToastProvider>
+    </ThemeProvider>
   );
 }
 
