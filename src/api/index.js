144e958421a1c861ec495a7369009bd8a79adf44
Colin Clark <bitdepth@users.noreply.github.com>
2021-06-07 09:36:19 +0100

    Fix upload issue

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,7 @@ export const uploadCats = (files) => {
     formData.append("sub_id", catSubId);
   });
 
-  fetch(`${API_URL}images/upload`, {
+  return fetch(`${API_URL}images/upload`, {
     method: "POST",
     headers: {
       "x-api-key": API_KEY
